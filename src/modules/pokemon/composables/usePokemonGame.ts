d79c56d9ef9589b990072ccab99822f131a5c699
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-28 13:42:37 -0700

    random pokemon

--- src/modules/pokemon/composables/usePokemonGame.ts
+++ src/modules/pokemon/composables/usePokemonGame.ts
@@ -7,6 +7,10 @@ export const usePokemonGame = () => {
   const pokemons = ref<Pokemon[]>([]);
   const pokemonOptions = ref<Pokemon[]>([]);
 
+  const randomPokemon = computed(() => {
+    const randomIndex = Math.floor(Math.random() * pokemonOptions.value.length);
+    return pokemonOptions.value[randomIndex];
+  });
   const isLoading = computed(() => pokemons.value.length === 0);
 
   const getPokemons = async (): Promise<Pokemon[]> => {
@@ -37,10 +41,12 @@ export const usePokemonGame = () => {
     pokemons.value = await getPokemons();
     getNextOptions();
   });
+
   return {
     gameStatus,
     isLoading,
     pokemonOptions,
+    randomPokemon,
 
     //Methods
     getNextOptions,
